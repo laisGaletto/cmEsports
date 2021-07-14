8859faed524ebf046c13a7b6cc86e15b54016dbb
lais galetto <laisgaletto@gmail.com>
2021-07-14 02:29:47 -0300

    deu certo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -104,15 +104,3 @@
 	})()
 
 })(jQuery);
-
-var intervalo;
-
-function scrollDireita(){
-  intervalo = setInterval(function(){ document.getElementById('scroller').scrollLeft += 1 }  , 5);
-};
-function scrollEsquerda(){
-  intervalo = setInterval(function(){ document.getElementById('scroller').scrollLeft -= 1 }  , 5);
-};
-function clearScroll(){
-  clearInterval(intervalo);
-};
